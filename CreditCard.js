bc05f04a611cb6438f10b360bb370a78b7993d04
kevinche75 <47882198+kevinche75@users.noreply.github.com>
2019-12-26 00:38:09 +0300

    CreditCard
    Initial

---
+++ CreditCard.js
@@ -0,0 +1,18 @@
+import {React} from 'react' 
+
+export class CreditCard extends React.Component{
+
+
+   consrtuctor(props){
+      super(props) 
+      state = {
+         number = '', 
+         data = '', 
+         owner = ''
+      }
+   }
+
+   render() {
+      return 
+   }
+}
