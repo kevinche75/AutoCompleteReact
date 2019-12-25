f85b491ae90dd7a2e85b729bc9fbd734d71729fe
kevinche75 <47882198+kevinche75@users.noreply.github.com>
2019-12-26 00:49:02 +0300

    Add

--- CreditCard.js
+++ CreditCard.js
@@ -8,11 +8,31 @@ export class CreditCard extends React.Component{
       state = {
          number = '', 
          data = '', 
-         owner = ''
-      }
+         owner = '', 
+         CVV = ''
+      } 
+//Так же все методы надо привязать,чтобы использовать this в них, приведу один
+      this.onChange = this.onChange.bind(this) 
+   }
+ 
+//Все аналогичны, напишу один
+   onChange(e) {
+//Вот тут делаете валидацию или добавляете / если это Data 
+      this.state.number = e.value
+   }
+ 
+   onClick(e) {
+      sendData(this.state.number, this.state.date, this.state.CVV, this.state.owner) 
    }
 
+
    render() {
-      return 
+      return(
+      <input onChange={this.onChangeNumber(event)} value={this.state.number}/>
+      <input onChange={this.onChangeData(event)} value={this.state.data}/>
+      <input onChange={this.onChangeOwner(event)} value={this.state.owner}/>
+      <input onChange={this.onChangeCVV(event)} value={this.state.CVV}/>
+      <button onClick={this.onClick() }/>
+      ) 
    }
 }
